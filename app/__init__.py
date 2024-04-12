7e37da3884591d412f9b84cbf60466279b19befa
hexoren21 <hexoren21@gmail.com>
2024-04-12 16:44:07 +0200

    corrected modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .models import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from . import routes
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+from app.models import User  # Move this import to the bottom of the file
+from . import routes  # Ensure routes are imported after models
