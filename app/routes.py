9227e9cbb5f8c77ec4b2916f03ffa1b2d4748608
hexoren21 <hexoren21@gmail.com>
2024-04-12 10:07:51 +0200

    change window style

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import render_template, request, redirect, url_for
+from . import app, db
+from .models import User
+from flask_login import login_user, login_required, logout_user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+        return '<h1>Invalid username or password</h1>'
+    return render_template('login.html')
+
+@app.route('/')
+@login_required
+def index():
+    return '<h1>Welcome to the To Do App</h1>'
